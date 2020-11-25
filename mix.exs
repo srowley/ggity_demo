ab6d037ff14abb9953e47646b1f532ef31566043
Steve Rowley <srowley@gmail.com>
2020-11-25 15:58:57 -0600

    use ggity hex package

--- mix.exs
+++ mix.exs
@@ -42,7 +42,7 @@ defmodule GGityDemo.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:ggity, git: "https://github.com/srowley/ggity.git"},
+      {:ggity, "~> 0.3.0"},
       {:nimble_csv, "~> 0.7"},
       {:makeup_elixir, "~> 0.15"}
     ]
